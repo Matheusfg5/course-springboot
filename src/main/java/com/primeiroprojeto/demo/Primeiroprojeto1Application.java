36f16295f9adb19fde1cc5af8ad74d2a394eadca
Matheus Goncalves <matheus_f_gocalves@outlook.com>
2020-04-12 19:06:04 -0300

    Project Created

---
+++ src/main/java/com/primeiroprojeto/demo/Primeiroprojeto1Application.java
@@ -0,0 +1,13 @@
+package com.primeiroprojeto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Primeiroprojeto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Primeiroprojeto1Application.class, args);
+	}
+
+}
